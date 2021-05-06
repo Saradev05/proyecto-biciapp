63a17b09fbf1ec76a2566b4591199ac077311684
aleidagonzalez <aleida1983@hotmail.com>
2021-05-06 14:11:43 +0000

    nueva rama error al compilar, return fuera de function

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -7,6 +7,8 @@ import "bootstrap/dist/css/bootstrap.css";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 //import your own components
 import Layout from "./layout";
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import Slider from "react-slick";
 
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import CostaIMG from "../../img/costa01.jpg";
@@ -9,52 +10,33 @@ import FamilyIMG from "../../img/family_home.jpg";
 import { Link } from "react-router-dom";
 import "../../styles/home.scss";
 
-export const Home = () => {
-	const { store } = useContext(Context);
-	return (
-		<div className="jumbotron-fluid">
-			<div className="container-fluid">
-				<img src={FamilyIMG} className="family-img" />
-			</div>
-
-			<div className="container py-5 my-5">
-				<div className="row text-center ">
-					<div className="col mx-auto mb-3">
-						<h2 className="text-muted ">PROXIMAS ACTIVIDADES</h2>
-					</div>
-				</div>
-				<div className="card-deck">
-					<div className="card">
-						<img className="card-img-top" src={FamilyIMG} alt="Card image cap" />
-						<div className="card-body">
-							<h5 className="card-title text-center">Eventos</h5>
-							<Link to={"/events"}>
-								<button className="btn btn-outline-warning">ver detalles</button>
-							</Link>
-						</div>
-					</div>
-
-					<div className="card">
-						<img className="card-img-top" src={MontanaIMG} alt="Card image cap" />
-						<div className="card-body">
-							<h5 className="card-title text-center">En Montaña</h5>
-							<Link to={"/montain"}>
-								<button className="btn btn-outline-warning">ver detalles</button>
-							</Link>
-						</div>
-					</div>
-
-					<div className="card">
-						<img className="card-img-top" src={CostaIMG} alt="Card image cap" />
-						<div className="card-body">
-							<h5 className="card-title text-center">En Costa</h5>
-							<Link to={"/beach"}>
-								<button className="btn btn-outline-warning">ver detalles</button>
-							</Link>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+const settings = {
+	dots: true,
+	infinite: true,
+	speed: 500,
+	slidesToShow: 1,
+	slidesToScroll: 1
 };
+return (
+	<div className="text-center mt-5 carousel-item">
+		<Slider {...settings}>
+			<div className="carousel-item">
+				<h3>
+					<img src=" ../../img/costa01.jpg" />
+				</h3>
+			</div>
+
+			<div className="carousel-item">
+				<h3>
+					<img src=" ../../img/montana0.jpg" />
+				</h3>
+			</div>
+
+			<div className="carousel-item">
+				<h3>
+					<img src=" ../../img/family_home.jpg" />
+				</h3>
+			</div>
+		</Slider>
+	</div>
+);
