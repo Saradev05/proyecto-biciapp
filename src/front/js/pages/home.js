b27856423f647d8df3be74094f9e28debbef0b86
aleidagonzalez <aleida1983@hotmail.com>
2021-05-12 13:20:38 +0000

    integracion stripe

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,9 +8,38 @@ import FamilyIMG from "../../img/family_home.jpg";
 import Cyclists from "../../img/cyclists.jpg";
 import { Link } from "react-router-dom";
 import "../../styles/home.scss";
+import { func } from "prop-types";
+
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+	"pk_test_51IqFlLJgftEGfFd1R1qoeXMJO3P8k7NWFVRsoYXqpKxUwg56CZw0GWa8WvzCleG1tgfepkUWX4vlU7K4Hoyyj2CL008bQepEHT"
+);
 
 export const Home = () => {
 	const { store } = useContext(Context);
+
+	async function handleClick() {
+		const stripe = await stripePromise;
+
+		const response = await fetch("https://3001-red-bass-pgvwzi28.ws-eu04.gitpod.io/api/create-checkout-session", {
+			method: "POST"
+		});
+
+		const session = await response.json();
+
+		// When the customer clicks on the button, redirect them to Checkout.
+
+		const result = await stripe.redirectToCheckout({
+			sessionId: session.id
+		});
+
+		if (result.error) {
+			// If `redirectToCheckout` fails due to a browser or network
+			// error, display the localized error message to your customer
+			// using `result.error.message`.
+		}
+	}
 	return (
 		<div className="jumbotron-fluid">
 			<div className="container-fluid p-0 ">
@@ -26,6 +55,21 @@ export const Home = () => {
 				visitar la web ya que vamos actualizando rutas a menudo...
 			</p>
 			<p className="text-unete">¡¡¡¡ÚNETE A BICIAPP!!!!</p>
+
+			<div className="container text-center mt-5" />
+			<div className="product">
+				<img src="https://i.imgur.com/EHyR2nP.png" alt="The cover of Stubborn Attachments" />
+
+				<div className="description">
+					<h3>Stubborn Attachments</h3>
+
+					<h5>$20.00</h5>
+				</div>
+			</div>
+
+			<button type="button" id="checkout-button" role="link" onClick={handleClick}>
+				Checkout
+			</button>
 
 			<div className="container py-5 my-5">
 				<div className="row text-center ">
