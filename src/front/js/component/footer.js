2335aef053515faf10670da1eedd5a47587fa320
Saradev05 <devyanand@gmail.com>
2021-05-16 21:53:28 +0000

    footer sin css

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,18 +4,31 @@ import { Context } from "../store/appContext";
 export const Footer = () => {
 	const { actions } = useContext(Context);
 	return (
-		<footer className="container mt-auto py-3 text-center  ">
-			<h5 className="d-flex align-items-center">
-				Made by
+		// <footer className="container-fluid d-flex py-3 content-center justify-content: space-around">
+		// 	<div className="col-4 text-justify ">
+		// 		<h5 className="row align-items-center">
+		// 			Made by
+		// 			<a className="p-2" href="mailto:info@aleidagonzalez.com">
+		// 				Aleida
+		// 			</a>
+		// 			<i className="p-2 fa fa-bicycle text-success fa-2x  " />
+		// 			<a className="p-2 ml-2" href="mailto:devyanand@gmail.com">
+		// 				Sara
+		// 			</a>
+		// 		</h5>
+		// 	</div>
+		// </footer>
+		<footer className="footer container mt-auto py-3 text-center">
+			<h5 className="row ml-50 align-items-center">
+				Made by{" "}
 				<a className="p-2" href="mailto:info@aleidagonzalez.com">
-					Aleida
+					Aleida{" "}
 				</a>
-				<i className="p-2 fa fa-bicycle text-success fa-2x  " />
-				<a className="p-2 " href="mailto:devyanand@gmail.com">
+				<i className="p-2 fa fa-bicycle text-success fa-2x  " />{" "}
+				<a className="p-2 ml-2" href="mailto:devyanand@gmail.com">
 					Sara{" "}
-				</a>
+				</a>{" "}
 			</h5>
-			<p />
 		</footer>
 	);
 };
